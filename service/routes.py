de5c751fb7c73fd866fd11ba4abb2badf8ec4f69
yulinhu1129 <yulinhu@umich.edu>
2023-03-06 17:24:38 -0500

    update delete code

--- service/routes.py
+++ service/routes.py
@@ -75,6 +75,24 @@ def create():
 
 
 ######################################################################
+# DELETE A RECOMMENDATION
+######################################################################
+@app.route("/recommendation/<int:recommendation_id>", methods=["DELETE"])
+def delelte(recommendation_id):
+    """ Create a new recommendation """
+
+    app.logger.info("Request to delete a Recommendation")
+    check_content_type("application/json")
+
+    # Delete the recommendation
+    rec = Recommendation.find(recommendation_id)
+    if rec:
+        rec.deserialize(request.get_json())
+        rec.delete()
+    
+    return make_response( "", status.HTTP_204_NO_CONTENT)
+
+######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
